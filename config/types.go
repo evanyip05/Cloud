1e287f2ee439b6907c4b8bb28257a41d779f3544
Evan Yip <92695816+evanyip05@users.noreply.github.com>
2023-11-07 10:33:21 -0800

    New type names

--- config/types.go
+++ config/types.go
@@ -2,15 +2,27 @@ package config
 
 import "go.mongodb.org/mongo-driver/bson"
 
-type Payload struct {
+// through the url rn
+type MongoGetRequest struct {}
+
+// type to send to mongo container (should be the only type in the db, or seperate different types by mongo dbs or collections)
+// stringify json data
+type MongoPutRequest struct {
 	Name string `json:"name"`
 	Data string `json:"data"`
 }
 
-type MongoID struct {
+type MongoPutResponse struct {
 	ID string `json:"id"`
 }
 
-type MongoEntries struct {
+type MongoGetResponse struct {
 	Entries []bson.M `json:"entries"`
 }
+
+/*
+
+mongo server response
+mongo server request
+
+*/
